28c60351080d33ca2c9f4adb2f1633f7ad6a052d
LobeTita <ajonglobe@gmail.com>
2024-12-14 00:41:53 +0100

    add ec2 instance and update

---
+++ ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "web2" {
+ ami     = "ami-0453ec754f44f9a4a"
+ instance_type = "t2.micro"
+ tags = {
+  Name = "HelloWorld"
+ }
+}
+terraform {
+ required_providers {
+  aws = {
+   source = "hashicorp/aws"
+   version = "~> 5.0"
+  }
+ }
+}
+# Configure the AWS Provider
+provider "aws" {
+ region = "us-east-1"
+ assume_role {
+  role_arn = "arn:aws:iam::654654615218:role/EC2Admin"
+ }
+}
---
+++ s3.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "terraform01" {
+  bucket = "256-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    TF_managed = true
+  }
+}
